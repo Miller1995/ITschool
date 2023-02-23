84158d550c30587c5c454ca3b30962ff5d5b72de
miller1995 <nircaanton95@gmail.com>
2023-02-23 21:54:46 +0200

    created 3 new java class and in these class placed the conditions for problems

---
+++ src/challengeString/ConvertNumberToString.java
@@ -0,0 +1,19 @@
+package challengeString;
+
+public class ConvertNumberToString {
+
+    public static void main(String[] args) {
+
+                   /*
+                    We need a function that can transform a number (integer) into a string.
+                    What ways of achieving this do you know?
+                    Examples (input --> output):
+                    123  --> "123"
+                    999  --> "999"
+                    -100 --> "-100"             */
+
+
+
+
+    }
+}
---
+++ src/challengeString/ReverseVowelsString.java
@@ -0,0 +1,25 @@
+package challengeString;
+
+public class ReverseVowelsString {
+
+    public static void main(String[] args) {
+
+        /*  In this kata, your goal is to write a function which will reverse the vowels in a string.
+            Any characters which are not vowels should remain in their original position.
+            Here are some examples:
+            reverseVowels("Hello!"); ---> "Holle!"
+            reverseVowels("Tomatoes"); ---> "Temotaos"
+            reverseVowels("Reverse Vowels In A String"); ---> "RivArsI Vewols en e Streng"
+            For simplicity, you can treat the letter y as a consonant, not a vowel.         */
+
+
+
+    }
+
+    public String reverseVowels(String message){
+
+
+
+        return "";
+    }
+}

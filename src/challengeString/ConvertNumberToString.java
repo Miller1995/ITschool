c179507769ca93a6485662d643d977690f163920
miller1995 <nircaanton95@gmail.com>
2023-02-26 15:35:13 +0200

    - created new solution for transform integer in string;

--- src/challengeString/ConvertNumberToString.java
+++ src/challengeString/ConvertNumberToString.java
@@ -1,5 +1,7 @@
 package challengeString;
 
+import java.util.Scanner;
+
 public class ConvertNumberToString {
 
     public static void main(String[] args) {
@@ -12,8 +14,11 @@ public class ConvertNumberToString {
                     999  --> "999"
                     -100 --> "-100"             */
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
 
-
-
+        String numberString = String.valueOf(number);
+        System.out.println("The string number is " + numberString);
     }
 }

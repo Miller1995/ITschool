d39cf81dc280b2462c4b4c0d22184ec145f0010b
miller1995 <nircaanton95@gmail.com>
2023-02-26 20:31:22 +0200

    - add new solution for this problem used 2 method 1 - check the vowel and 2 - revers the vowel;

--- src/challengeString/ReverseVowelsString.java
+++ src/challengeString/ReverseVowelsString.java
@@ -12,14 +12,41 @@ public class ReverseVowelsString {
             reverseVowels("Reverse Vowels In A String"); ---> "RivArsI Vewols en e Streng"
             For simplicity, you can treat the letter y as a consonant, not a vowel.         */
 
-
+        String string = "Dont worry, be happy";
+        System.out.println(reverseVowels(string));
 
     }
 
-    public String reverseVowels(String message){
+    // function for check the vowel
+    public static boolean isVowels(char c){
+        return (c == 'a' || c == 'A' ||
+                c == 'e' || c == 'E' ||
+                c == 'i' || c == 'I' ||
+                c == 'o' || c == 'O' ||
+                c == 'u' || c == 'U' );
+    }
+
+    // function for revers the position of vowels
+    public static String reverseVowels(String str){
+        char[] arrayVowels = str.toCharArray();
+        int position = 0;
+        String vowel = "";
 
 
+        // sort the vowel
+        for (int index = 0; index < arrayVowels.length; index++){
+            if (isVowels(arrayVowels[index])){                               // identify the vowel in array after index
+                position++;                                                  // if the vowel is finding, position increment, initial  position = 0 -> 1 -> 2 -> 3 -> 4
+                vowel = vowel + arrayVowels[index];                          // in string vowel add a char(vowel), initial vowel = "" -> "o" -> "oo" -> "ooe" -> "ooea"
+            }
+        }
 
-        return "";
+        for (int index = 0; index < arrayVowels.length; index++){
+            if (isVowels(arrayVowels[index])){                              // identify the vowel in array after index
+                position--;                                                 // after find the vowel decrement position
+                arrayVowels[index] = vowel.charAt(position);                // put the vowel after position in place where was found the vowel
+            }
+        }
+        return String.valueOf(arrayVowels);
     }
 }

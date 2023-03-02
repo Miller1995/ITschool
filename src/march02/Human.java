97d937a2cefc9d3f475f4d4dbade1cad2f47babb
miller1995 <nircaanton95@gmail.com>
2023-03-02 22:39:52 +0200

    - march 02 lesson

---
+++ src/march02/Human.java
@@ -0,0 +1,15 @@
+package march02;
+
+public class Human {
+
+    private String sex;
+    private double height;
+    private double weight;
+
+    void work(){}
+    void walk(){}
+
+    public Human getHuman(){
+        return new Human();
+    }
+}
